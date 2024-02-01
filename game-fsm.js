d1fb9390dcb8a0e07b3654827b96795ac0fc119f
Kristopher38 <Kristopher38@wp.pl>
2024-02-01 18:34:39 +0100

    Implement game logic in the backend, game comms between backend and frontend and ghost disc drawing

--- game-fsm.js
+++ game-fsm.js
@@ -2,19 +2,100 @@ export class GameFSM {
     constructor(room, socket) {
         this.room = room
         this.roomSocket = socket
+        this.board = [
+            [-1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1],
+        ]
+        this.currentPlayer = -1
     }
 
+    _firstFree(colIdx) {
+        return this.board[colIdx].findIndex(v => v == -1)
+    }
+
+    _colFull(colIdx) {
+        return this.board[colIdx][5] !== -1
+    }
+
+    _placeDisc(playerIdx, colIdx) {
+        this.board[colIdx][self._firstFree(colIdx)] = playerIdx
+    }
+
+    _checkWinCondition() {
+        // column win check
+        for (let start = 0; start < 3; start++) {
+            // for every column
+            for (let i = 0; i < 7; i++) {
+                // select 4 elements from a column
+                const slice = this.board[i].slice(start, start+4)
+                // check that they're nonempty and identical
+                if (slice[0] != -1 && slice.every(v => v == slice[0]))
+                    return slice[0]
+            }
+        }
+
+        for (let start = 0; start < 4; start++) {
+            // row win check
+            // select 4 columns
+            const slice = this.board.slice(start, start+4)
+            // for every row in a column
+            for (let j = 0; j < 6; j++)
+                // check that they're nonempty and identical
+                if (slice[0][j] != -1 && slice.every(col => col[j] == slice[0][j]))
+                    return slice[0][j]
+
+            // diagonal win check
+            for (let j = 0; j < 3; j++)
+                // for every left-to-right upward diagonal start height check
+                // if it's nonempty and all elements identical
+                if (slice.every((col, idx) => col[j+idx] == slice[0][j]))
+                    return slice[0][j]
+                // for every left-to-right downward diagonal start height check
+                // if it's nonempty and all elements identical
+                if (slice.every((col, idx) => col[j+3-idx] == slice[0][j+3]))
+                    return slice[0][j+3]
+        }
+    }
 
     async start() { 
         this.playerSockets = await this.roomSocket.fetchSockets()
-        console.log(this.playerSockets)
-        this.roomSocket.emit("start")
+
+        // register appropriate handlers for events sent by players
+        // once the game has started
+        this.playerSockets.forEach(playerSocket, playerIdx => {
+            playerSocket.on("move", colIdx => this.move(playerIdx, colIdx))
+            playerSocket.emit("start", playerIdx)
+        })
+
         this.turn(0)
     }
 
-    turn(player) {
-        this.playerSockets[player].emit("your turn")
-        
-        // if (winCondition) {}
+    move(playerIdx, colIdx) {
+        if (this.currentPlayer === playerIdx && !this._colFull(colIdx)) {
+            const nextPlayerIdx = (playerIdx + 1) % this.room.players.length
+            this._placeDisc(playerIdx, colIdx)
+            this.roomSocket("game update", this.board, nextPlayerIdx)
+
+            const winner = this._checkWinCondition()
+            if (winner > -1)
+                this.end(winner)
+            else
+                this.turn(nextPlayerIdx)
+        }
+    }
+
+    end(winnerIdx) {
+        this.roomSocket.emit("finished", winnerIdx)
+        this.playerSockets.forEach(playerSocket => playerSocket.off("move"))
+    }
+
+    turn(playerIdx) {
+        this.currentPlayer = playerIdx
+        this.playerSockets[playerIdx].emit("your turn")
     }
 }
--- public/game.js
+++ public/game.js
@@ -1,5 +1,17 @@
 const socket = io("/room")
 
+const discStyles = ["d-red", "d-yellow"]
+let ourPlayerIdx = 0
+let board = [
+    [-1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1],
+]
+
 function renderPlayerList(players) {
     const playerNames = document.querySelectorAll(".player-name")
     for (let i = 0; i < playerNames.length; i++) {
@@ -7,8 +19,34 @@ function renderPlayerList(players) {
     }
 }
 
+function highlightPlayer(playerIdx) {
+
+}
+
+function renderBoard(board) {
+
+}
+
 document.querySelector("#start-button").addEventListener("click", function() {
     socket.emit("start request")
+})
+
+document.querySelectorAll(".column").forEach((column, colIdx) => {
+    const topmost = board[colIdx].findIndex(v => v == -1)
+    if (topmost > -1) {
+        const childIdx = 5 - topmost
+        column.addEventListener("mouseenter", ev => {
+            column.children[childIdx].classList.add("disc", "ghost", discStyles[ourPlayerIdx])
+        })
+        column.addEventListener("mouseleave", ev => {
+            if (board[colIdx][childIdx] === -1)
+                column.children[childIdx].classList.remove("disc", discStyles[ourPlayerIdx])
+            column.children[childIdx].classList.remove("ghost")
+        })
+        column.addEventListener("click", ev => {
+            socket.emit("move", colIdx)
+        })
+    }
 })
 
 socket.on("connect", async () => {
@@ -23,10 +61,21 @@ socket.on("room update", room => {
     renderPlayerList(room.players)
 })
 
-socket.on("start", () => {
+socket.on("start", yourPlayerIdx => {
     console.log("Game has started")
+    ourPlayerIdx = yourPlayerIdx
 })
 
 socket.on("your turn", () => {
-    
+    console.log("It's your turn")
+})
+
+socket.on("game update", (newBoard, currentPlayerIdx) => {
+    board = newBoard
+    renderBoard(board)
+    highlightPlayer(currentPlayerIdx)
+})
+
+socket.on("finished", winnerIdx => {
+    console.log(`Game finished, player ${winnerIdx} won`)
 })
